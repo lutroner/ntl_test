0e6139f7886bdd843091eebe45dfe797e33fb387
Alexander <dubplate@ya.ru>
2022-10-14 23:07:11 +0300

    Add get_token function

--- main.py
+++ main.py
@@ -4,20 +4,45 @@ from dotenv import load_dotenv
 import argparse
 import pandas as pd
 from datetime import datetime
+import json
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
-
-TOKEN = os.environ.get("TOKEN")
+PAYLOAD = {
+    "video_auth_token": "A",
+    "uuid": "A",
+    "mobile": False,
+    "device_info": {}
+}
+# TOKEN = os.environ.get("TOKEN")
+USERNAME = os.environ.get("FF_USER")
+PASSWORD = os.environ.get("PASSWORD")
 OBJECTS_PER_PAGE = 100
-FINDFACE_ENDPOINT = "https://demo.ntechlab.com/events/faces/"
-FINDFACE_CARD_ENDPOINT = "https://demo.ntechlab.com/cards/humans/"
+FINDFACE_ENDPOINT = "https://demo.ntechlab.com/"
 DEFAULT_CARD_ID = "839"
-DEFAULT_EVENT_ID = "4469884292355629956"
 COLUMNS_FOR_REPORT = ('episode', 'matched_card', 'person_name',
                       'created_date', 'thumbnail', 'fullframe', 'confidence',
                       'camera', 'camera_group')
+
+
+def get_token_from_findface(url, user, password, payload):
+    endpoint_url = f"{url}auth/login/"
+    headers = {'content-type': 'application/json'}
+    payload = json.dumps(payload)
+    res = requests.post(endpoint_url, auth=(user, password),
+                        data=payload, headers=headers)
+    return res.json()['token']
+
+
+def save_token_to_env_file(token):
+    with open(".env", 'a') as f:
+        f.write(f"\nTOKEN={token}")
+
+
+def is_token_in_env():
+    with open(".env") as f:
+        return "TOKEN" in f.read()
 
 
 def fetch_data_from_findface(endpoint_url, params):
@@ -28,21 +53,23 @@ def fetch_data_from_findface(endpoint_url, params):
 
 def get_human_name_by_card_id(token, url, card_id):
     params = {'token': token}
-    endpoint_url = f"{url}{card_id}"
+    endpoint_url = f"{url}cards/humans/{card_id}"
     return fetch_data_from_findface(endpoint_url, params)["name"]
 
 
 def fetch_data_by_card_id(token, url, card_id):
     params = {'token': token, "matched_card": card_id,
               'limit': OBJECTS_PER_PAGE}
-    return fetch_data_from_findface(url, params)
+    endpoint_url = f"{url}events/faces/"
+    return fetch_data_from_findface(endpoint_url, params)
 
 
 def fetch_data_by_event_id(token, url, event_id):
     params = {'token': token,
               "looks_like": f"faceevent:{event_id}",
               'limit': OBJECTS_PER_PAGE}
-    return fetch_data_from_findface(url, params)
+    endpoint_url = f"{url}events/faces/"
+    return fetch_data_from_findface(endpoint_url, params)
 
 
 def create_excel_report(data, person_name="N/A"):
@@ -63,9 +90,17 @@ if __name__ == '__main__':
     cli_parser.add_argument('--event_id', nargs='?', help='ID события')
     card_id = cli_parser.parse_args().card_id
     event_id = cli_parser.parse_args().event_id
+    # Если токена нет в .env файле, то запрашиваем новый и записываем его в файл
+    if not is_token_in_env():
+        save_token_to_env_file(get_token_from_findface(url=FINDFACE_ENDPOINT,
+                                                       password=PASSWORD,
+                                                       user=USERNAME,
+                                                       payload=PAYLOAD))
+    load_dotenv()
+    TOKEN = os.environ.get("TOKEN")
     if card_id and not event_id:
         person_name = get_human_name_by_card_id(token=TOKEN,
-                                                url=FINDFACE_CARD_ENDPOINT,
+                                                url=FINDFACE_ENDPOINT,
                                                 card_id=card_id)
         data = fetch_data_by_card_id(token=TOKEN,
                                      url=FINDFACE_ENDPOINT,
